3c6dee8f64e4b4d06741944aa9c8c4bd8511ebfc
Ryan Fitz-Gerald <ztif33@gmail.com>
2025-08-14 14:58:59 -0400

    Added Feedforward object generators to FFSettings

--- src/main/java/frc/lib2960/settings/FFSettings.java
+++ src/main/java/frc/lib2960/settings/FFSettings.java
@@ -1,6 +1,8 @@
 package frc.lib2960.settings;
 
-
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 
 public class FFSettings {
 
@@ -44,6 +46,19 @@ public class FFSettings {
      */
     public FFSettings(double kS, double kV) {
         this(kS, 0, kV, 0);
+    }
+
+
+    public SimpleMotorFeedforward getSimpleMotorFF(){
+        return new SimpleMotorFeedforward(kS, kV, kA);
+    }
+
+    public ArmFeedforward getArmFF() {
+        return new ArmFeedforward(kS, kG, kV, kA);
+    }
+
+    public ElevatorFeedforward getElevatorFF() {
+        return new ElevatorFeedforward(kS, kG, kV, kA);
     }
 
 }
