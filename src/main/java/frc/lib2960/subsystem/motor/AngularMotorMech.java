b288069948986809b164249f3e1b67e96416792e
Ryan Fitz-Gerald <ztif33@gmail.com>
2025-09-03 10:07:07 -0400

    Added auto-complete commands for motor mechanisms

--- src/main/java/frc/lib2960/subsystem/motor/AngularMotorMech.java
+++ src/main/java/frc/lib2960/subsystem/motor/AngularMotorMech.java
@@ -13,6 +13,7 @@ import edu.wpi.first.units.measure.MutAngularVelocity;
 import edu.wpi.first.units.measure.MutVoltage;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib2960.config.subsystem.AngularMotorMechConfig;
 import frc.lib2960.config.subsystem.MotorMechCommonConfig.LimitTrim;
@@ -23,6 +24,8 @@ import frc.lib2960.controller.AngularController;
  */
 public abstract class AngularMotorMech extends SubsystemBase {
     // TODO Implement SysID
+    // TODO Implement Telemetry
+    // TODO Implement Logging
     public final AngularMotorMechConfig config;
 
     private final AngularController controller;
@@ -105,40 +108,132 @@ public abstract class AngularMotorMech extends SubsystemBase {
 
         if (config.common.limitTrim == LimitTrim.Voltage)
             controller.trimVoltage(curPos, targetVolt);
-            
+
         setMotorVoltage(volts);
     }
 
+    /**
+     * Sets the output voltage for all the motors
+     * @param volts output voltage for all the motors
+     */
     public abstract void setMotorVoltage(Voltage volts);
 
+    /**
+     * Gets the current position of the mechanism.
+     * @param result    mutable object to store the result
+     */
     public abstract void getPosition(MutAngle result);
 
+    /**
+     * Gets the current velocity of the mechanism.
+     * @param result    mutable object to store the result
+     */
     public abstract void getVelocity(MutAngularVelocity result);
 
+    /**
+     * Gets the current voltage applied to each motor on the mechanism
+     * @param result    mutable object to store the result
+     */
     public abstract void getVoltage(MutVoltage result);
-
 
     /*********************/
     /* Command Factories */
     /*********************/
-    public Command getPositionCmd(Angle position) {
-        return this.run(() -> this.gotoPosition(position));
+    /**
+     * Gets a command that moves to a target position
+     * @param target    target position
+     * @return  new command to move to a target position
+     */
+    public Command getPositionCmd(Angle target) {
+        return this.run(() -> this.gotoPosition(target));
     }
 
-    public Command getVelocity(Supplier<AngularVelocity> velocity) {
-        return this.run(() -> this.gotoVelocity(velocity.get()));
+    /**
+     * Gets a command that moves to a target position. Command finishes automatically when the current position is within tolerance of the target position.
+     * @param target    target position
+     * @param tolerance target position tolerance
+     * @return  new command to move to a target position
+     */
+    public Command getPositionCmd(Angle target, Angle tolerance) {
+        return Commands.deadline(
+                getAtTargetCmd(target, tolerance),
+                getPositionCmd(target));
     }
 
-    public Command getVelocity(AngularVelocity velocity) {
-        return this.run(() -> this.gotoVelocity(velocity));
+    /**
+     * Gets a command that moves to a target velocity
+     * @param target    target velocity supplier
+     * @return  new command to move to a target velocity
+     */
+    public Command getVelocity(Supplier<AngularVelocity> target) {
+        return this.run(() -> this.gotoVelocity(target.get()));
     }
 
-    public Command getVoltage(Supplier<Voltage> velocity) {
-        return this.run(() -> this.setVoltage(velocity.get()));
+    /**
+     * Gets a command that moves to a target velocity
+     * @param target    target velocity
+     * @return  new command to move to a target velocity
+     */
+    public Command getVelocity(AngularVelocity target) {
+        return this.run(() -> this.gotoVelocity(target));
     }
 
-    public Command getVoltage(Voltage velocity) {
-        return this.run(() -> this.setVoltage(velocity));
+
+    /**
+     * Gets a command that moves to a target velocity. Command finishes automatically when the current velocity is within tolerance of the target velocity.
+     * @param target    target velocity
+     * @param tolerance target velocity tolerance
+     * @return  new command to move to a target velocity
+     */
+    public Command getVelocity(AngularVelocity target, AngularVelocity tolerance) {
+        return Commands.deadline(
+                getAtTargetCmd(target, tolerance),
+                getVelocity(target));
     }
 
+    /**
+     * Gets a command that moves to a target voltage
+     * @param target    target voltage supplier
+     * @return  new command to move to a target voltage
+     */
+    public Command getVoltage(Supplier<Voltage> target) {
+        return this.run(() -> this.setVoltage(target.get()));
+    }
+
+    /**
+     * Gets a command that moves to a target voltage
+     * @param target    target voltage
+     * @return  new command to move to a target voltage
+     */
+    public Command getVoltage(Voltage target) {
+        return this.run(() -> this.setVoltage(target));
+    }
+
+    /**
+     * Gets a command that ends when the mechanism is within tolerance of the target velocity.
+     * @param target    target position
+     * @param tolerance target position tolerance
+     * @return
+     */
+    public Command getAtTargetCmd(Angle target, Angle tolerance) {
+        return Commands.waitUntil(
+                () -> {
+                    getPosition(curPos);
+                    return target.isNear(curPos, tolerance);
+                });
+    }
+
+    /**
+     * Gets a command that ends when the mechanism is within tolerance of the target position.
+     * @param target    target velocity
+     * @param tolerance target velocity tolerance
+     * @return  new command to move to a target velocity
+     */
+    public Command getAtTargetCmd(AngularVelocity target, AngularVelocity tolerance) {
+        return Commands.waitUntil(
+                () -> {
+                    getVelocity(curVel);
+                    return target.isNear(curVel, tolerance);
+                });
+    }
 }
