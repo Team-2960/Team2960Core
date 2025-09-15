891447c6c25b6aee6983ce50dc9750abbc7aad9c
Ryan Fitz-Gerald <ztif33@gmail.com>
2025-09-14 22:28:54 -0400

    Began Adding controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,18 +4,21 @@ package frc.robot;
 import static edu.wpi.first.units.Units.DegreesPerSecond;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.units.measure.MutAngularVelocity;
 import edu.wpi.first.units.measure.MutLinearVelocity;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.lib2960.helper.PathPlanner;
 import frc.lib2960.subsystem.drivetrain.swerve.NavXSwerveDrive;
 import frc.lib2960.subsystem.drivetrain.swerve.RevFlexMaxSwerveModule;
+import frc.robot.FieldLayout.ReefBranchOffset;
 import frc.robot.subsystems.AlgaeArm;
 import frc.robot.subsystems.AlgaeRoller;
 import frc.robot.subsystems.ArmElevControl;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.CoralArm;
 import frc.robot.subsystems.CoralRoller;
+import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Elevator;
 
 /**
@@ -25,60 +28,100 @@ import frc.robot.subsystems.Elevator;
 public class RobotContainer {
 
     // Subsystems
-    public static final RevFlexMaxSwerveModule[] swerveModules = {
+    public final RevFlexMaxSwerveModule[] swerveModules = {
             new RevFlexMaxSwerveModule(Constants.swerveModuleCommonConfig, Constants.lfConfig),
             new RevFlexMaxSwerveModule(Constants.swerveModuleCommonConfig, Constants.rfConfig),
             new RevFlexMaxSwerveModule(Constants.swerveModuleCommonConfig, Constants.lrConfig),
             new RevFlexMaxSwerveModule(Constants.swerveModuleCommonConfig, Constants.rrConfig),
     };
 
-    public static final NavXSwerveDrive drivetrain = new NavXSwerveDrive(Constants.swerveDriveConfig, swerveModules);
-    public static final CoralArm coralArm = new CoralArm(Constants.coralArmConfig, Constants.coralArmMotorConfig);
-    public static final CoralRoller coralRoller = new CoralRoller(Constants.coralRollerConfig);
-    public static final Elevator elevator = new Elevator(Constants.elevatorConfig, Constants.elevatorMotorConfig);
-    public static final ArmElevControl armElevControl = new ArmElevControl(coralArm, elevator, Constants.coralArmPosTol,
+    public final Drivetrain drivetrain = new Drivetrain(Constants.swerveDriveConfig,
+            swerveModules);
+    public final CoralArm coralArm = new CoralArm(Constants.coralArmConfig, Constants.coralArmMotorConfig);
+    public final CoralRoller coralRoller = new CoralRoller(Constants.coralRollerConfig);
+    public final Elevator elevator = new Elevator(Constants.elevatorConfig, Constants.elevatorMotorConfig);
+    public final ArmElevControl armElevControl = new ArmElevControl(coralArm, elevator,
+            Constants.coralArmPosTol,
             Constants.elevatorPosTol);
-    public static final AlgaeArm algaeArm = new AlgaeArm(Constants.algaeArmConfig, Constants.algaeArmMotorConfig);
-    public static final AlgaeRoller algaeRoller = new AlgaeRoller(Constants.algaeRollerConfig);
-    public static final Climber climber = new Climber(Constants.climberConfig);
+    public final AlgaeArm algaeArm = new AlgaeArm(Constants.algaeArmConfig, Constants.algaeArmMotorConfig);
+    public final AlgaeRoller algaeRoller = new AlgaeRoller(Constants.algaeRollerConfig);
+    public final Climber climber = new Climber(Constants.climberConfig);
+    public final  
 
     // Joysticks
-    public static final CommandXboxController driveCtrl = new CommandXboxController(0);
-    public static final CommandXboxController opCtrl = new CommandXboxController(1);
+    public final CommandXboxController driveCtrl = new CommandXboxController(Constants.driverCtrlID);
+    public final CommandXboxController opCtrl = new CommandXboxController(Constants.opCtrlID);
 
     // Helper Units
-    private static final MutLinearVelocity xTeleopVel = MetersPerSecond.mutable(0);
-    private static final MutLinearVelocity yTeleopVel = MetersPerSecond.mutable(0);
-    private static final MutAngularVelocity rTeleopVel = DegreesPerSecond.mutable(0);
+    private final MutLinearVelocity xTeleopVel = MetersPerSecond.mutable(0);
+    private final MutLinearVelocity yTeleopVel = MetersPerSecond.mutable(0);
+    private final MutAngularVelocity rTeleopVel = DegreesPerSecond.mutable(0);
 
     // TODO Implement AprilTag vision
     // TODO Map Joysticks
 
     /**
-     * Static initializer
+     * initializer
      */
-    static {
-        initControls();
+    public RobotContainer() {
+        // Init Driver Controls
+        initDrivetrainCtrl();
+        initCoralGripperCtrl();
+
+        // Init PathPlanner
         initPathPlanner();
     }
 
-    private static void initControls() {
-        // Init Drivetrain controls
+    private final void initDrivetrainCtrl() {
+        // Init Teleop Drivetrain Controls
         drivetrain.setDefaultCommand(drivetrain.getVelocityControlCmd(
-                () -> xTeleopVel.mut_replace(-driveCtrl.getLeftX() * Constants.driveLinMaxVel.in(MetersPerSecond),
+                () -> xTeleopVel.mut_replace(
+                        MathUtil.applyDeadband(-driveCtrl.getLeftX(), Constants.linDriveDB)
+                                * Constants.driveLinMaxVel.in(MetersPerSecond),
                         MetersPerSecond),
-                () -> yTeleopVel.mut_replace(-driveCtrl.getLeftY() * Constants.driveLinMaxVel.in(MetersPerSecond),
+                () -> yTeleopVel.mut_replace(
+                        MathUtil.applyDeadband(-driveCtrl.getLeftY(), Constants.linDriveDB)
+                                * Constants.driveLinMaxVel.in(MetersPerSecond),
                         MetersPerSecond),
-                () -> rTeleopVel.mut_replace(-driveCtrl.getRightX() * Constants.driveAngMaxVel.in(DegreesPerSecond),
+                () -> rTeleopVel.mut_replace(
+                        MathUtil.applyDeadband(-driveCtrl.getRightX(), Constants.angDriveDB)
+                                * Constants.driveAngMaxVel.in(DegreesPerSecond),
                         DegreesPerSecond)));
 
+        // Map Auto-Align
+        driveCtrl.y().whileTrue(
+                drivetrain.getGotoReefCmd(
+                        ReefBranchOffset.MIDDLE,
+                        Constants.coralXOffset,
+                        Constants.coralYOffset,
+                        Constants.coralROffset));
+
+        driveCtrl.y().and(driveCtrl.rightBumper()).whileTrue(
+                drivetrain.getGotoReefCmd(
+                        ReefBranchOffset.RIGHT,
+                        Constants.coralXOffset,
+                        Constants.coralYOffset,
+                        Constants.coralROffset));
+
+        driveCtrl.y().and(driveCtrl.leftBumper()).whileTrue(
+                drivetrain.getGotoReefCmd(
+                        ReefBranchOffset.LEFT,
+                        Constants.coralXOffset,
+                        Constants.coralYOffset,
+                        Constants.coralROffset));
+    }
+
+    private final void initCoralCtrl() {
+        // Map ArmElev Presets
+        opCtrl.y().onTrue(Elev)
     }
 
     /**
      * Initializes path planner
      */
-    private static void initPathPlanner() {
+    private void initPathPlanner() {
         PathPlanner.initPathPlanner(drivetrain);
         // TODO Put auto chooser on driver station
     }
+
 }
